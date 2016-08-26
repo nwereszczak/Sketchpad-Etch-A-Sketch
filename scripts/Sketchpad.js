d29f68d061e4340c821d7a20cee90e5a03a1d6b4
Nicholas Wereszczak <nwereszczak@gmail.com>
2016-08-26 14:33:55 -0400

    cells change color/opacity when you hover

--- scripts/Sketchpad.js
+++ scripts/Sketchpad.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+	buildGrid();
+
+	var $cell = $(".cell");
+	$cell.mouseenter(function() {
+		if( !($(this).attr("class") === "cell max") ) {
+			$(this).css("background-color", "black");
+			var op = $(this).css("opacity");
+			op = parseFloat(op, 10);
+			if( op === 1 ) op = 0;
+			op = (op+0.1).toString();
+			console.log(op);
+			if( op === "1") $(this).addClass("max");
+			$(this).css("opacity", op);
+		}
+	});
+});
+
+function buildGrid() {
 	var $grid = $('#grid tbody');
-	var blockForGrid = "<td> <div></div> </td>";
+	var blockForGrid = "<td> <div class='cell'></div> </td>";
 	for( var x=0; x<16; x++ ) {
 		$grid.append("<tr></tr>");
 		var row = $("#grid tbody:last-child");
@@ -8,4 +26,8 @@ $(document).ready(function() {
 			row.append(blockForGrid);
 		}
 	}
-});
+}
+
+function reset() {
+	
+}
